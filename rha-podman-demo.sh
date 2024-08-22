a809407a82837ee797be6ca7430027ee766a9a94
Student User <student@workstation.lab.example.com>
2024-08-22 01:10:13 -0400

    Adding Colour to Podman Script

--- rha-podman-demo.sh
+++ rha-podman-demo.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+BIRed='\033[1;91m' 
+BBlue='\033[1;34m'
+RED='\033[0;31m'
+BGreen='\033[1;32m'
+NC='\033[0m' # No Color
 
-echo -e "Pulling the image from Quay.io repository and Creating Container \n"
+echo -e "${BIRed}Pulling the image from Quay.io repository and Creating Container prompt.${NC}\n"
 
 podman run --name rha-podman-demo -d -P  quay.io/dbais76/rha-demo:1.0
 
@@ -14,7 +19,7 @@ else
   sleep 5 
 fi
 
-echo "Now Opening application page in Firefox Browser" 
+echo -e "${BBlue}Now Opening application page in Firefox Browser prompt.${NC}\n" 
 PAGE=`podman  port rha-podman-demo | grep 8080 | awk '{print $3}'`
 
 firefox $PAGE
